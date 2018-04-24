a0d9e4edf1be5465cac0e478819aa621834d03b5
hangtingchen <465244243@qq.com>
2018-04-24 23:23:56 +0800

    fix bugs

--- chenhangting/script/lstmBaseline/baselinednn_weight_early_stopping.py
+++ chenhangting/script/lstmBaseline/baselinednn_weight_early_stopping.py
@@ -101,43 +101,54 @@ def resize_batch(data,label,length,name):
             data_new=data[inx,0:l,:]
             label_new=label[inx,0:l]
         else:
-            data_new=np.array([data_new,data[inx,0:l,:])
-            label_new=label([label_new,label[inx,0:l])
+            data_new=np.array([data_new,data[inx,0:l,:]])
+            label_new=label([label_new,label[inx,0:l]])
     data=torch.FloatTensor(data)
     label=torch.LongTensor(label)
     return (data,label,length,name)
 
 class Net(nn.Module):
-    def __init__(self,input_dim,hidden_dim,output_dim,num_layers,biFlag,dropout=0.5):
+    def __init__(self,input_dim,hidden_dim,output_dim,dropout=0.5):
         #dropout
         super(Net,self).__init__()
         self.input_dim=input_dim
         self.hidden_dim=hidden_dim
         self.output_dim=output_dim
-        self.num_layers=num_layers
-        if(biFlag):self.bi_num=2
-        else:self.bi_num=1
-        self.biFlag=biFlag
 
-        self.layer1=nn.LSTM(input_size=input_dim,hidden_size=hidden_dim, \
-                        num_layers=num_layers,batch_first=True, \
-                        dropout=dropout,bidirectional=biFlag)
-        # out = (len batch outdim) ?
+        self.layer1=nn.Sequential(
+            nn.Linear(self.input_dim,self.hidden_dim),
+            nn.BatchNorm1d(self.hidden_dim),
+            nn.ReLU(),
+        )
         self.layer2=nn.Sequential(
-            nn.Linear(hidden_dim*self.bi_num,output_dim),
-            nn.LogSoftmax(dim=2)
+            nn.Linear(self.hidden_dim,self.hidden_dim),
+            nn.BatchNorm1d(self.hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+        )
+        self.layer3=nn.Sequential(
+            nn.Linear(self.hidden_dim,self.hidden_dim),
+            nn.BatchNorm1d(self.hidden_dim),
+            nn.ReLU(),
+        )
+        self.layer4=nn.Sequential(
+            nn.Linear(self.hidden_dim,self.hidden_dim),
+            nn.BatchNorm1d(self.hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(p=dropout)
+        )
+        self.layer5=nn.Sequential(
+            nn.Linear(self.hidden_dim,self.output_dim),
+            nn.LogSoftmax(dim=1),
         )
 
-
-    def init_hidden(self,batch_size):
-        return (Variable(torch.zeros(self.num_layers*self.bi_num,batch_size,self.hidden_dim)).cuda(),
-                Variable(torch.zeros(self.num_layers*self.bi_num,batch_size,self.hidden_dim)).cuda())
     def forward(self,x,batch_size):
-        hidden1=self.init_hidden(batch_size)
-        out,hidden1=self.layer1(x,hidden1)
-        out,length=pad_packed_sequence(out,batch_first=True)
+        out=self.layer1(x)
         out=self.layer2(out)
-        return out,length
+        out=self.layer3(out)
+        out=self.layer4(out)
+        out=self.layer5(out)
+        return out
 
 model=Net(**superParams)
 model.cuda()
