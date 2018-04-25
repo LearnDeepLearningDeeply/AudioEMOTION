5a879150a514a22cfc531d2a6e5448c716de37b9
hangtingchen <465244243@qq.com>
2018-04-25 20:49:24 +0800

    finish baseline system

--- chenhangting/script/metrics/log_metric.py
+++ chenhangting/script/metrics/log_metric.py
@@ -13,7 +13,8 @@ PATHlogroot=sys.argv[1]
 
 acc=[];fscore=[]
 for i in range(num_folds):
-    PATHlog="{}{}.log".format(PATHlogroot,i)
+    fold=i+1
+    PATHlog="{}{}.log".format(PATHlogroot,fold)
     with open(PATHlog,'r') as f:
         temp_acc='';temp_fscore=''
         for line in f:
@@ -25,8 +26,8 @@ for i in range(num_folds):
 acc=np.array(acc)
 fscore=np.array(fscore)
 if(len(acc)==num_folds and len(fscore)==num_folds):
-    print("Average acc {}-+{}".format(np.mean(acc),np.std(acc,ddof=1)))
-    print("Average marco f-score {}-+{}".format(np.mean(fscore),np.std(fscore,ddof=1)))
+    print("Average acc {:.03f}-+{:.03f}".format(np.mean(acc),np.std(acc,ddof=1)))
+    print("Average marco f-score {:.03f}-+{:.03f}".format(np.mean(fscore),np.std(fscore,ddof=1)))
 else:
     print(acc)
     print(fscore)
